47d21219b9bb5b2a95b8fc1019811ad69657e7fc
Alessandro Limardo <alessandro.limardo@kaleyra.com>
2025-07-01 17:22:29 +0200

    First commit

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "KaleyraVideoNoiseFilter",
+    platforms: [ .iOS(.v15) ],
+    products: [
+        .library(name: "KaleyraVideoNoiseFilter", targets: ["KaleyraVideoNoiseFilter"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/KaleyraVideo/iOSDeepFilterNet.git", .upToNextMinor(from: "0.0.39")),
+        .package(url: "https://github.com/nschum/SwiftHamcrest.git", from: "2.2.4"),
+        .package(url: "https://github.com/KaleyraVideo/KaleyraTestKit.git", branch: "master")
+    ],
+    targets: [
+        .target(
+            name: "KaleyraVideoNoiseFilter", dependencies: [
+                .product(name: "DeepFilterNet", package: "iOSDeepFilterNet"),
+            ],
+            resources: [
+                .process("Resources")
+            ]),
+        .testTarget(
+            name: "KaleyraVideoNoiseFilterTests",
+            dependencies: [
+                .target(name: "KaleyraVideoNoiseFilter"),
+                .product(name: "SwiftHamcrest", package: "SwiftHamcrest"),
+                .product(name: "KaleyraTestKit", package: "KaleyraTestKit"),
+                .product(name: "KaleyraTestMatchers", package: "KaleyraTestKit"),
+                .product(name: "KaleyraTestHelpers", package: "KaleyraTestKit"),
+            ]
+        ),
+    ]
+)
---
+++ Tests/KaleyraVideoNoiseFilterTests/DeepFilterTests.swift
@@ -0,0 +1,128 @@
+// Copyright © 2018-2025 Kaleyra S.p.a. All Rights Reserved.
+// See LICENSE for licensing information
+
+import Foundation
+import SwiftHamcrest
+import KaleyraTestKit
+import KaleyraTestHelpers
+import KaleyraTestMatchers
+import DeepFilterNet
+@testable import KaleyraVideoNoiseFilter
+
+final class DeepFilterTests: UnitTestCase {
+
+    // MARK: - Test Setup
+
+    private var sut: DeepFilter!
+    private var deepFilterNetSpy: DeepFilterNetSpy!
+    private var buffer: UnsafeMutablePointer<Int16>!
+
+    override func setUp() {
+        super.setUp()
+
+        deepFilterNetSpy = makeDeepFilterNetSpy()
+        buffer = UnsafeMutablePointer<Int16>.allocate(capacity: 1)
+        sut = makeSUT(filter: deepFilterNetSpy)
+    }
+
+    override func tearDown() {
+        sut = nil
+        buffer.deallocate()
+        buffer = nil
+        deepFilterNetSpy = nil
+
+        super.tearDown()
+    }
+
+    // MARK: - Tests
+
+    func testInitShouldCallCreateStateOnDeepFilterNet() {
+        assertThat(deepFilterNetSpy.createStateInvocations, hasCount(1))
+        assertThat(deepFilterNetSpy.createStateInvocations.first?.channels, presentAnd(equalTo(1)))
+        assertThat(deepFilterNetSpy.createStateInvocations.first?.attenLim, presentAnd(equalTo(33.0)))
+    }
+
+    func testShouldSetSupportedFrameLengthUsingDeepFilterNet() {
+        assertThat(deepFilterNetSpy.getFrameLengthInvocations, equalTo([deepFilterNetSpy.mockedState]))
+        assertThat(sut.supportedFrameLength, equalTo(512))
+    }
+
+    func testRemoveNoiseFromBufferShouldCallProcessFrame() {
+        let result = sut.removeNoise(from: buffer!, bufferSize: 512)
+
+        assertThat(deepFilterNetSpy.processFrameInvocations, hasCount(1))
+        assertThat(deepFilterNetSpy.processFrameInvocations.first?.state, presentAnd(equalTo(deepFilterNetSpy.mockedState)))
+        assertThat(deepFilterNetSpy.processFrameInvocations.first?.input, presentAnd(equalTo(buffer)))
+        assertThat(deepFilterNetSpy.processFrameInvocations.first?.frameSize, presentAnd(equalTo(512)))
+        assertThat(result, equalTo(0.75))
+    }
+
+    func testRemoveNoiseFromBufferProvidingAMismatchingBufferTypeShouldReturnNegativeOne() {
+        let result = sut.removeNoise(from: NSObject(), bufferSize: 512)
+
+        assertThat(result, equalTo(-1))
+    }
+
+    func testRemoveNoiseFromBufferProvidingAMismatchingBufferSizeShouldReturnNegativeOne() {
+        let result = sut.removeNoise(from: buffer!, bufferSize: NSObject())
+
+        assertThat(result, equalTo(-1))
+    }
+
+    func testDeinitShouldFreeState() {
+        sut = nil
+
+        assertThat(deepFilterNetSpy.freeStateInvocations, hasCount(1))
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(filter: DeepFilterNetProtocol) -> DeepFilter {
+        .init(filter: filter)
+    }
+
+    private func makeDeepFilterNetSpy() -> DeepFilterNetSpy {
+        .init()
+    }
+
+    // MARK: - Doubles
+
+    private class DeepFilterNetSpy: DeepFilterNetProtocol {
+
+        let mockedState: OpaquePointer = OpaquePointer(bitPattern: 0x1000)!
+
+        private(set) var createStateInvocations = [(modelBytes: UnsafePointer<UInt8>, modelSize: Int32, channels: Int32, attenLim: Float)]()
+        private(set) var processFrameInvocations = [(state: OpaquePointer, input: UnsafePointer<Int16>, frameSize: Int32)]()
+        private(set) var getFrameLengthInvocations = [OpaquePointer]()
+        private(set) var setAttenLimInvocations = [(state: OpaquePointer, limDb: Float)]()
+        private(set) var setPostFilterBetaInvocations = [(state: OpaquePointer, beta: Float)]()
+        private(set) var freeStateInvocations = [OpaquePointer]()
+
+        func createState(modelBytes: UnsafePointer<UInt8>, modelSize: Int32, channels: Int32, attenLim: Float) -> OpaquePointer? {
+            createStateInvocations.append((modelBytes: modelBytes, modelSize: modelSize, channels: channels, attenLim: attenLim))
+            return mockedState
+        }
+
+        func processFrame(state: OpaquePointer, input: UnsafePointer<Int16>, frameSize: Int32) -> Float {
+            processFrameInvocations.append((state: state, input: input, frameSize: frameSize))
+            return 0.75
+        }
+
+        func getFrameLength(state: OpaquePointer) -> Int32 {
+            getFrameLengthInvocations.append(state)
+            return 512
+        }
+
+        func setAttenLim(state: OpaquePointer, limDb: Float) {
+            setAttenLimInvocations.append((state: state, limDb: limDb))
+        }
+
+        func setPostFilterBeta(state: OpaquePointer, beta: Float) {
+            setPostFilterBetaInvocations.append((state: state, beta: beta))
+        }
+
+        func freeState(state: OpaquePointer) {
+            freeStateInvocations.append(state)
+        }
+    }
+}
